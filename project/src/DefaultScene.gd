b26f4c6e74fe2d5104960a81d6d8bbb9e90fec71
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-16 11:14:34 -0400

    Fix ok button

--- project/src/DefaultScene.gd
+++ project/src/DefaultScene.gd
@@ -8,12 +8,6 @@ func _on_OnScreenKeyboard_number_pressed(number):
 	answer_label.add_character(number)
 
 
-func _on_Control_answer_changed(answer):
-	_problem = $ProblemGenerator.problem
-	if answer == str(_problem):
-		print("Yay")
-
-
 func _on_OnScreenKeyboard_backspace_pressed():
 	answer_label.backspace()
 
