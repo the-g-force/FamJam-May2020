f9bb734efc0c8db0efbe559692e4c88dcba1d895
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-16 10:53:33 -0400

    Add backspace to label

--- project/src/DefaultScene.gd
+++ project/src/DefaultScene.gd
@@ -2,12 +2,17 @@ extends Control
 
 var _problem
 
+onready var answer_label : Answer = $Answer
+
 func _on_OnScreenKeyboard_number_pressed(number):
-	# For now, just echo the key to the console to show it works
-	print(number)
+	answer_label.add_character(number)
 
 
 func _on_Control_answer_changed(answer):
 	_problem = $ProblemGenerator.problem
 	if answer == str(_problem):
 		print("Yay")
+
+
+func _on_OnScreenKeyboard_backspace_pressed():
+	answer_label.backspace()
--- project/src/DynamicNumberLabel.gd
+++ project/src/DynamicNumberLabel.gd
@@ -1,9 +1,16 @@
 extends Control
+class_name Answer
 
 onready var _answer : Label = $Label
 signal answer_changed(answer)
 
 
-func _on_OnScreenKeyboard_number_pressed(number):
+func add_character(number):
 	_answer.text += str(number)
+	emit_signal("answer_changed", _answer.text)
+
+
+func backspace():
+	var last_cha = _answer.text.length()
+	_answer.text = _answer.text.substr(0, last_cha-1)
 	emit_signal("answer_changed", _answer.text)
