48e476940679484f8c06f49d3f6b724bf8c1d72c
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-05-16 14:33:09 -0400

    Add credits scene and integrate into main menu navigation

--- project/src/TitleScene.gd
+++ project/src/TitleScene.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var GameScene := preload("res://src/DefaultScene.tscn")
+var CreditsScene := preload("res://src/CreditsScene.tscn")
 
 
 func _on_StartButton_pressed():
@@ -9,3 +10,7 @@ func _on_StartButton_pressed():
 
 func _on_FullscreenButton_pressed():
 	OS.window_fullscreen = not OS.window_fullscreen
+
+
+func _on_CreditsButton_pressed():
+	var _ignored = get_tree().change_scene_to(CreditsScene)
