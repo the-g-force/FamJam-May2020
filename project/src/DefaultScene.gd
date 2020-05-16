e6830af196a305ebf446cadc0422fe35f54dca49
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-16 13:32:47 -0400

    Add random operators

--- project/src/DefaultScene.gd
+++ project/src/DefaultScene.gd
@@ -26,5 +26,4 @@ func _on_OnScreenKeyboard_ok_pressed():
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	$OnScreenKeyboard.disabled = false
 	$Answer/Label.text = ""
-	if _feedback.text == "Yay":
-		$ProblemGenerator.generate()
+	$ProblemGenerator.generate()
--- project/src/ProblemGenerator.gd
+++ project/src/ProblemGenerator.gd
@@ -5,15 +5,29 @@ var _answer : String = ""
 var _problem_text : String = ""
 var _operator : String = ""
 var problem
+var type : int
 var _first_digit : int
 var _second_digit : int
 
 func generate():
 	randomize()
+	type = int(round(rand_range(1,4)))
 	_first_digit = int(round(rand_range(1,10)))
 	_second_digit = int(round(rand_range(1,10)))
-	problem = _first_digit*_second_digit
-	_operator = " x "
+	if type == 1:
+		problem = _first_digit*_second_digit
+		_operator = " x "
+	elif type == 2:
+		problem = _first_digit+_second_digit
+		_operator = " + "
+	elif type == 3:
+		problem = _first_digit
+		_first_digit *= _second_digit
+		_operator = "/"
+	elif type == 4:
+		problem = _first_digit
+		_first_digit += _second_digit
+		_operator = " - "
 	_problem_text = str(_first_digit) + _operator + str(_second_digit) + " = "
 	_text.text = _problem_text
 
