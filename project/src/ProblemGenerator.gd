6c69dcf531f2df28267aa803b83aea7c79443ff6
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-05-16 09:43:30 -0400

    Add problem generator script

---
+++ project/src/ProblemGenerator.gd
@@ -0,0 +1,20 @@
+extends Node
+
+onready var _text : Label = $Label
+var _problem_text : String = ""
+var _operator : String = ""
+var _problem
+var _first_digit : int
+var _second_digit : int
+
+func generate():
+	randomize()
+	_first_digit = int(round(rand_range(1,10)))
+	_second_digit = int(round(rand_range(1,10)))
+	_problem = _first_digit*_second_digit
+	_operator = " x "
+	_problem_text = str(_first_digit) + _operator + str(_second_digit) + " = "
+	_text.text = _problem_text
+
+func _ready():
+	generate()
