7185d1af72d35dab965959b154f4519305c4e1d0
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-05-16 15:12:28 -0400

    Add room tracker and win scene

--- project/src/DefaultScene.gd
+++ project/src/DefaultScene.gd
@@ -1,11 +1,13 @@
 extends Control
 
 var _death_scene := preload("res://src/DeathScene.tscn")
+var WinScene := preload("res://src/WinScene.tscn")
 
 onready var _answer : Answer = $HBoxContainer/Answer
 onready var _problem = $HBoxContainer/ProblemGenerator
 onready var _feedback : Label = $Label
 onready var _feedback_animatior : AnimationPlayer = $AnimationPlayer
+onready var _room_tracker := $RoomTracker
 
 
 func _on_OnScreenKeyboard_number_pressed(number):
@@ -27,6 +29,10 @@ func _on_OnScreenKeyboard_ok_pressed():
 
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
+	if _room_tracker.is_at_end():
+		var _ignored = get_tree().change_scene_to(WinScene)
+	else:
+		_room_tracker.advance()
 	$OnScreenKeyboard.disabled = false
 	_answer.text = ""
 	if $HealthTracker.health > 0:
---
+++ project/src/RoomTracker.gd
@@ -0,0 +1,22 @@
+extends Label
+
+const LAST_ROOM := 10
+
+var current_room : int = 1
+
+func _ready():
+	_update_text()
+
+
+func _update_text():
+	text = "Room %d of %d" % [current_room, LAST_ROOM]
+	
+
+func advance():
+	current_room += 1
+	_update_text()
+	
+	
+func is_at_end() -> bool:
+	return current_room == LAST_ROOM
+	
