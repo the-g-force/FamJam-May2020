327ca9d7d21dbaf96aafc778ee420426c0581efa
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-05-16 10:38:40 -0400

    Add backspace and OK button to OSK

--- project/src/OnScreenKeyboard.gd
+++ project/src/OnScreenKeyboard.gd
@@ -1,11 +1,15 @@
 extends Control
 
 signal number_pressed(number)
+signal backspace_pressed
+signal ok_pressed
+
 
 func _ready():
 	var buttons = _get_all_buttons()
 	for button in buttons:
 		button.connect('pressed', self, '_on_number_button_clicked', [button])
+
 	
 func _get_all_buttons():
 	var rows = [$VBoxContainer/Row1, $VBoxContainer/Row2]
@@ -15,5 +19,14 @@ func _get_all_buttons():
 			buttons.append(child)
 	return buttons
 
+
 func _on_number_button_clicked(button):
 	emit_signal('number_pressed', button.text)
+
+
+func _on_BackspaceButton_pressed():
+	emit_signal("backspace_pressed")
+
+
+func _on_CommitButton_pressed():
+	emit_signal("ok_pressed")
